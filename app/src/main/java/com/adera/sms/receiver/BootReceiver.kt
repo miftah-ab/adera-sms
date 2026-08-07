746a755bf256fde3ea7c9a546201c958575a9942
miftah-ab <fuhdjj7@gmail.com>
2026-08-06 22:10:57 -0700

    Fix missing SupervisorJob import in BootReceiver

--- app/src/main/java/com/adera/sms/receiver/BootReceiver.kt
+++ app/src/main/java/com/adera/sms/receiver/BootReceiver.kt
@@ -8,6 +8,7 @@ import com.adera.sms.data.AppDatabase
 import com.adera.sms.service.CallMonitorService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 
 /**
